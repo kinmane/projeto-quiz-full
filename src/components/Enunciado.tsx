b7c9f75d007d6fb801277849a70df42f0fd449ab
Gabriel Souza <rorato91@gmail.com>
2023-08-28 15:36:22 -0300

    finalizado

--- src/components/Enunciado.tsx
+++ src/components/Enunciado.tsx
@@ -1,15 +1,13 @@
 import styles from '../styles/Enunciado.module.css'
 
 interface EnunciadoProps {
-	texto: string
+    texto: string
 }
 
 export default function Enunciado(props: EnunciadoProps) {
-	return (
-		<div className={styles.enunciado}>
-			<div className={styles.texto}>
-				<span className={styles.texto}>{props.texto}</span>
-			</div>
-		</div>
-	)
+    return (
+        <div className={styles.enunciado}>
+            <span className={styles.texto}>{props.texto}</span>
+        </div>
+    )
 }
--- src/pages/api/questoes/[id].ts
+++ src/pages/api/questoes/[id].ts
@@ -1,15 +1,13 @@
-import questoes from "../bancoDeQuestoes";
+import questoes from '../bancoDeQuestoes'
+export default function questoesProId(req, res) {
+    const idSelecionado = +req.query.id
 
-export default (req, res) => {
-	const idSelecionado = +req.query.id
+    const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
 
-	const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
-
-	if (unicaQuestaoOuNada.length === 1) {
-		const questaoSelecionada = unicaQuestaoOuNada[0].embaralharRespostas()
-		const obj = questaoSelecionada.responderCom(0).paraObjeto()
-		res.status(200).json(obj)
-	} else {
-		res.status(204).send()
-	}
+    if (unicaQuestaoOuNada.length === 1) {
+        const questaoSelecionada = unicaQuestaoOuNada[0].embaralharRespostas()
+        res.status(200).json(questaoSelecionada.paraObjeto())
+    } else {
+        res.status(204).send()
+    }
 }
